48e0b81572cc73cce067039d90fd96582d63d17f
Michael <michaelvoov@yandex.ru>
2023-10-28 08:53:55 +0500

    Fixed and  Update

--- Quantorium/main.py
+++ Quantorium/main.py
@@ -72,6 +72,7 @@ class User(db.Model):
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.Text(), nullable=False)
     pathUser = db.Column(db.Integer, nullable=False)
+    is_super_user = db.Column(db.Boolean, default=False, nullable=False)
     date = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
@@ -371,16 +372,13 @@ def comment_update(id):
         comment.title = request.form['title']
         comment.text = request.form['text']
 
-        if user != comment.author:
-            return redirect('/404')
-
         try:
             db.session.commit()
             return redirect('/products')
         except:
             return "Опа"
     else:
-            return render_template("products_update.html", comment=comment, user=user)
+            return render_template("products_update.html", comment=comment)
             
 @app.route('/create_feedback', methods=["POST", "GET"])
 def create_feedback():

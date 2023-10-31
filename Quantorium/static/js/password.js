f8b079e124b5835b8ee044377c8b5714553d7a9f
Michael <michaelvoov@yandex.ru>
2023-10-31 23:47:02 +0500

    Update

--- Quantorium/static/js/password.js
+++ Quantorium/static/js/password.js
@@ -1,9 +1,9 @@
-$('body').on('click', '.apavolku-galocksed', function(){
-    if ($(this).is(':checked')){
-    $('#Password1').attr('type', 'text');
-    $('#Password2').attr('type', 'text');
-    } else {
-    $('#Password1').attr('type', 'password');
-    $('#Password2').attr('type', 'password');
+function checking(){
+    var password = document.querySelector('#password');
+    if ( password.getAttribute('type') == 'password'){
+        password.setAttribute('type', 'text');
     }
-  });
+    else{
+        password.setAttribute('type', 'password');
+    }
+}

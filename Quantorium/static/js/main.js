28acc094a20ab0f96013aed03624e9592abdec5b
Michael <michaelvoov@yandex.ru>
2023-11-01 07:49:42 +0500

    2

--- Quantorium/static/js/main.js
+++ Quantorium/static/js/main.js
@@ -1,14 +1,13 @@
 const toggleButton = document.getElementById('toggle-button');
 const body = document.body;
-const header = document.header;
 
 const darkMode = localStorage.getItem('darkMode');
 
 if (darkMode) {
-    body, header.classList.add('dark-mode');
+    body.classList.add('dark-mode');
 }
 
 toggleButton.onclick = function() {
-    body, header.classList.toggle('dark-mode');
-    localStorage.setItem('darkMode', body, header.classList.contains('dark-mode'));
+    body.classList.toggle('dark-mode');
+    localStorage.setItem('darkMode', body.classList.contains('dark-mode'));
 }
